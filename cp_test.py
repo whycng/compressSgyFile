e9ea066e2aea3989cee2894f993f4d904611eb6f
whycng <yrvv7s@sina.com>
2024-07-15 20:59:23 +0800

    单道，某些道提取给实现了

--- cp_test.py
+++ cp_test.py
@@ -15,10 +15,16 @@ from scipy.interpolate import interp1d
 from scipy.signal import convolve2d
 from scipy.ndimage import zoom
 import pywt
+import joblib
+
+'''
+设置卷积核，降采样，pca特征保留率
+'''
 
 tracecount = -1
 gob_shape = (-1, -1)
-downsample_factor = 7
+downsample_factor = 8
+pca_model_file = r"PCA_Model.pkl"
 
 def read_grid_file(grid_file):
     """
@@ -48,6 +54,11 @@ def read_grid_file(grid_file):
 
     return df_headers, df_data
 
+def pca_compress(data, n_components=100):
+    pca = PCA(n_components=n_components)
+    compressed_data = pca.fit_transform(data)
+    return compressed_data, pca
+
 def compress_segy(input_file, output_file, compression_rate):
     """
     压缩 SEGY 数据。
@@ -57,10 +68,12 @@ def compress_segy(input_file, output_file, compression_rate):
         output_file: 输出压缩文件文件名。
         compression_rate: 压缩率，取值范围为 0 到 1，表示保留的特征比例。
     """
+    global pca_model_file
 
     # 加载 SEGY 数据
     with segyio.open(input_file, ignore_geometry=True) as segy_file:
         data = segy_file.trace.raw[:] # 获取所有道的数据
+        global tracecount
         tracecount = segy_file.tracecount # 获取道的总数
         samples_per_trace = segy_file.samples.size # 获取每道的样本数
         segy_format = int(segy_file.format)  # 获取SEGY文件的格式, 确保格式为整数类型
@@ -74,7 +87,7 @@ def compress_segy(input_file, output_file, compression_rate):
         print("compress--- gob_shape:", gob_shape)
 
     # 定义卷积核
-    kernel_size = 5
+    kernel_size = 8
     kernel = np.ones((kernel_size, kernel_size)) / (kernel_size * kernel_size)
 
     # 对数据进行2D卷积（平滑处理）
@@ -93,38 +106,16 @@ def compress_segy(input_file, output_file, compression_rate):
     print("Downsampled data shape:", downsampled_data.shape)
     data = downsampled_data
 
+    # 对二维数据进行PCA压缩
+    compressed_data, pca = pca_compress(data, n_components=85)
+    print("pca data shape:", compressed_data.shape)
+    # 存储压缩后的数据和PCA模型
+    np.savez(output_file, compressed_data=compressed_data, components=pca.components_, mean=pca.mean_,tracecount=tracecount, samples_per_trace=samples_per_trace, segy_format=segy_format)
 
+    joblib.dump(pca, pca_model_file)
+    print(f"Compressed data saved to {output_file}")
+    print(f"PCA model saved to {pca_model_file}")
 
-
-    # 降采样
-    def downsample(signal, factor):
-        return signal[::factor]
-
-    # # 对每个波形应用降采样
-    # downsample_factor = 10  # 降采样因子
-    # downsampled_data = np.array([downsample(waveform, downsample_factor) for waveform in data])
-    #
-    # # 打印原始数据和降采样后数据的形状
-    # print("Original data shape:", data.shape)
-    # print("Downsampled data shape:", downsampled_data.shape)
-    #
-    # # 可视化一个原始波形和降采样后的波形
-    # # plt.figure(figsize=(10, 4))
-    # # plt.subplot(1, 2, 1)
-    # # plt.plot(data[0])
-    # # plt.title("Original Waveform")
-    # # plt.subplot(1, 2, 2)
-    # # plt.plot(downsampled_data[0])
-    # # plt.title("Downsampled Waveform")
-    # # plt.show()
-    # data = downsampled_data
-
-    # 使用 PCA 进行降维
-    pca = PCA(n_components=compression_rate)
-    compressed_data = pca.fit_transform(data) # data
-
-    # 保存压缩后的数据和 PCA 对象
-    np.savez(output_file, compressed_data=compressed_data, components=pca.components_, mean=pca.mean_, tracecount=tracecount, samples_per_trace=samples_per_trace, segy_format=segy_format)
 
 def decompress_segy(input_file, output_file, start_trace, end_trace):
     """
@@ -136,6 +127,8 @@ def decompress_segy(input_file, output_file, start_trace, end_trace):
         start_trace: 解压缩的起始道号。
         end_trace: 解压缩的结束道号。
     """
+    global tracecount
+    global pca_model_file
 
     # 加载压缩后的数据和 PCA 对象
     npzfile = np.load(input_file)
@@ -146,37 +139,40 @@ def decompress_segy(input_file, output_file, start_trace, end_trace):
     samples_per_trace = npzfile['samples_per_trace']
     segy_format = npzfile['segy_format']
 
-    # 使用 PCA 进行重建
-    pca = PCA()
-    pca.components_ = components
-    pca.mean_ = mean
-    reconstructed_data = pca.inverse_transform(compressed_data)
+    # 加载压缩后的数据和PCA模型
+    pca = joblib.load(pca_model_file)
+    print(f"Loaded compressed data shape: {compressed_data.shape}")
+
+    global downsample_factor
+    # 提取指定范围的道
+    compressed_subset = compressed_data[(start_trace // downsample_factor) : ((end_trace // downsample_factor) + 1)]
+    # compressed_subset = compressed_data[start_trace:end_trace]
+    print(f"Compressed subset shape: {compressed_subset.shape}",start_trace // downsample_factor, (end_trace // downsample_factor) + 1 , "end_trace:",end_trace)
+
+    # 使用PCA模型还原指定范围的道
+    reconstructed_data = pca.inverse_transform(compressed_subset)
+    print(f"Reconstructed data shape: {reconstructed_data.shape}")
 
     # 插值恢复（升采样）
     def upsample2d(data, factor):
         return zoom(data, factor)
 
-    global downsample_factor
-    upsampled_data = upsample2d(reconstructed_data, downsample_factor)
+    def upsample(data, factor, original_shape):
+        # 使用缩放因子进行插值
+        upsampled_data = zoom(data, (factor, factor), order=1)
+
+        # 修剪或填充数据以匹配原始形状
+        upsampled_data = upsampled_data[:original_shape[0], :original_shape[1]]
+
+        return upsampled_data
+
+    # global downsample_factor
+    # upsampled_data = upsample2d(reconstructed_data, downsample_factor)
+    upsampled_data = upsample(reconstructed_data, downsample_factor, gob_shape)
 
     # 打印恢复后数据的形状
     print("Upsampled data shape:", upsampled_data.shape)
     reconstructed_data = upsampled_data
-
-
-    # 降采样恢复
-    # 插值恢复
-    # def upsample(signal, original_length):
-    #     x_original = np.linspace(0, original_length - 1, num=original_length)
-    #     x_downsampled = np.linspace(0, original_length - 1, num=len(signal))
-    #     interpolator = interp1d(x_downsampled, signal, kind='linear')
-    #     return interpolator(x_original)
-    #
-    # global gob_shape
-    # print("gob_shape:",gob_shape)
-    # # 对每个波形进行插值恢复
-    # reconstructed_data = np.array([upsample(waveform, gob_shape[1]) for waveform in reconstructed_data])
-    # print("reconstructed_data.shape: ", reconstructed_data.shape)
 
     # 创建 SEGY 文件规范
     spec = segyio.spec()
@@ -222,6 +218,8 @@ def plot_data(original_data, decompressed_data, start_trace, end_trace):
     plt.show()
 
 def fun_main(): # 主函数
+    global tracecount
+
     # 读取网格文件
     grid_file = r"E:\XueXiao\比赛-东方杯\2-compress\PostData_training-grid.dat"
     grid_header, grid_data = read_grid_file(grid_file)
@@ -231,12 +229,12 @@ def fun_main(): # 主函数
     # 示例用法
     ori_file = r"E:\XueXiao\比赛-东方杯\2-compress\PostData-for-training.sgy"
     comp_file = 'compressed_segy_data.npz'
-    compression_rate = 0.85 # 保留 95% 的特征
+    compression_rate = 0.89 # 保留 95% 的特征
     compress_segy(ori_file, comp_file, compression_rate)
 
     output_file = 'decompressed_segy_data.segy'
     start_trace = 0
-    end_trace = tracecount
+    end_trace = tracecount #int(tracecount / 5)
     decompress_segy(comp_file, output_file, start_trace, end_trace)
 
     # 加载原始数据
